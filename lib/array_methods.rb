604a431e59414f2c3205e996d14353de492aacfc
Rei M <r.clarkemitchell@gmail.com>
2019-09-05 08:48:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,15 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+   array.length.times do |index|
+	#  p array[index]
+     if array[index] === value_to_find
+		 return index
+		 else
+		 return nil
+		 end
+	 end
 end
 
+#scale = [2,3,4,5]
 def find_max_value(array)
   array.max()
 end
